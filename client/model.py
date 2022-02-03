9becc46a96597eef5766daeb864b3e54e2cec044
Spencer Churchill <spence@duck.com>
2022-02-02 23:32:27 -0800

    make model easier to import

--- client/model.py
+++ client/model.py
@@ -3,55 +3,48 @@ import numpy as np
 import torch
 import torch.nn.functional as f
 from transformers import BertModel, BertTokenizer
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-bert_version = 'bert-large-cased'
-tokenizer = BertTokenizer.from_pretrained(bert_version)
-model = BertModel.from_pretrained(bert_version)
-
-# TODO: import model into BentoML
-
-# TODO: show the reduced embedding space
+import bentoml
+from save_bento import load_model_to_bentoml
 
 
-def visualize(distances, figsize=(10, 5), titles=None):
-    plt.show()
+class BertModel:
+    def __init__(self):
+        self.model, self.tokenizer = \
+            self.load_model_from_bentoml()
+        self.device = self.get_device()
 
+    def load_model_from_bentoml(self):
+        try:
+            model = bentoml.pytorch.load('bert-model')
+            tokenizer = bentoml.pytorch.load('bert-tokenizer')
+        except FileNotFoundError:
+            load_model_to_bentoml()
+            model = bentoml.pytorch.load('bert-model')
+            tokenizer = bentoml.pytorch.load('bert-tokenizer')
+        return model, tokenizer
 
-# evalute the model instead of training
-model = model.eval()
-model = model.to(device)
+    def get_device(self):
+        return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-texts = [
-    'Have you gotten the API to work?',
-    'The API seems broken! HELP!',
-    'It seems like there\'s a bug in the API, I\'ll try fixing it soon!',
-    'I\'m having trouble getting the API to work!',
-    'I love the new UI',
-    'Who designed the UI? It looks wonderful!',
-]
+    def embed_responses(self, responses) -> np.array:
+        encodings = self.tokenizer(
+            responses,
+            padding=True,
+            return_tensors='pt'
+        ).to(self.device)
+        # disable gradient descent
+        with torch.no_grad():
+            embeds = self.model(**encodings)
+        embeds = embeds[0]
+        CLSs = embeds[:, 0, :]
+        # normalize the CLS token embeddings
+        normalized = f.normalize(CLSs, p=2, dim=1)
+        # cosine similarity
+        cls_dist = normalized.matmul(normalized.T)
+        cls_dist = cls_dist.new_ones(cls_dist.shape) - cls_dist
+        cls_dist = cls_dist.numpy()
+        return cls_dist
 
-encodings = tokenizer(
-    texts,
-    padding=True,
-    return_tensors='pt'
-).to(device)
-
-# disable gradient descent
-with torch.no_grad():
-    embeds = model(**encodings)
-embeds = embeds[0]
-
-CLSs = embeds[:, 0, :]
-
-# normalize the CLS token embeddings
-normalized = f.normalize(CLSs, p=2, dim=1)
-# cosine similarity
-cls_dist = normalized.matmul(normalized.T)
-cls_dist = cls_dist.new_ones(cls_dist.shape) - cls_dist
-cls_dist = cls_dist.numpy()
-
-# visualize
-
-visualize([cls_dist], titles=["CLS"])
+    def visualize(self, distances, figsize=(10, 5), titles=None):
+        # TODO: show the reduced embedding space
+        plt.show()
