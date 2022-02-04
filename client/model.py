a588d0abf46a4edae971d1cdba8e1753d6bb6ccb
Spencer Churchill <spence@duck.com>
2022-02-03 21:01:48 -0800

    overhaul visualization

--- client/model.py
+++ client/model.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 import torch
 import torch.nn.functional as f
 from sklearn.manifold import TSNE
-from sklearn import decomposition
+from sklearn.decomposition import PCA
 from transformers import BertModel, BertTokenizer
 import bentoml
 from save_bento import load_model_to_bentoml
@@ -14,8 +14,8 @@ class BertModel:
         self.model, self.tokenizer = \
             self.load_model_from_bentoml()
         self.device = self.get_device()
-        self.tsne = TSNE(n_components=2)
-        self.pca = decomposition.PCA(n_components=2)
+        self.tsne = TSNE(n_components=3)
+        self.pca = PCA(n_components=3)
 
     def load_model_from_bentoml(self):
         try:
@@ -53,4 +53,22 @@ class BertModel:
             XY = self.tsne.fit_transform(
                 self.embed_responses(responses)
             )
-        plt.scatter(*zip(*XY))
+        # basic coloring scheme
+        color = []
+        for response in responses:
+            if 'UI' in response:
+                color.append('red')
+            elif 'API' in response:
+                color.append('blue')
+            else:
+                color.append('green')
+        fig = px.scatter_3d(
+            XY, x=0, y=1, z=2,
+            color=color,
+            hover_data={
+                'response': responses
+            },
+            title=f'{type.upper()} visualization of responses',
+            labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3'}
+        )
+        fig.show()
