aaee36a2894454af382ac459cf81cde92ce84e5a
Spencer Churchill <spence@duck.com>
2022-02-03 16:45:49 -0800

    basic implementation of pca plotting

--- client/model.py
+++ client/model.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as f
+from sklearn.manifold import TSNE
+from sklearn import decomposition
 from transformers import BertModel, BertTokenizer
 import bentoml
 from save_bento import load_model_to_bentoml
@@ -12,6 +14,8 @@ class BertModel:
         self.model, self.tokenizer = \
             self.load_model_from_bentoml()
         self.device = self.get_device()
+        self.tsne = TSNE(n_components=2)
+        self.pca = decomposition.PCA(n_components=2)
 
     def load_model_from_bentoml(self):
         try:
@@ -35,16 +39,18 @@ class BertModel:
         # disable gradient descent
         with torch.no_grad():
             embeds = self.model(**encodings)
-        embeds = embeds[0]
-        CLSs = embeds[:, 0, :]
+        CLSs = embeds[0][:, 0, :]
         # normalize the CLS token embeddings
         normalized = f.normalize(CLSs, p=2, dim=1)
-        # cosine similarity
-        cls_dist = normalized.matmul(normalized.T)
-        cls_dist = cls_dist.new_ones(cls_dist.shape) - cls_dist
-        cls_dist = cls_dist.numpy()
-        return cls_dist
+        return normalized
 
-    def visualize(self, distances, figsize=(10, 5), titles=None):
-        # TODO: show the reduced embedding space
-        plt.show()
+    def visualize(self, responses, type='pca'):
+        if type == 'pca':
+            XY = self.pca.fit_transform(
+                self.embed_responses(responses)
+            )
+        elif type == 'tsne':
+            XY = self.tsne.fit_transform(
+                self.embed_responses(responses)
+            )
+        plt.scatter(*zip(*XY))
